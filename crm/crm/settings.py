ec364e52d4a9aef1cff6eee82f694e86f292aecc
Fowerus <fowerus@gmail.com>
2021-08-20 12:07:58 +0700

    v0.36

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -129,7 +129,7 @@ STATIC_ROOT = BASE_DIR / 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigIntegerField'
 
 AUTHENTICATION_BACKENDS = ('django.contrib.auth.backends.ModelBackend',) #'Users.CustomAuthBackend.AuthBackend'
 
