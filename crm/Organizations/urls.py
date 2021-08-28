a8c337fc29f026b83a391d854ae4b21118f232b8
Fowerus <fowerus@gmail.com>
2021-08-29 01:46:01 +0700

    0.951

--- crm/Organizations/urls.py
+++ crm/Organizations/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
         'patch':'update_organization_number',
         'delete':'delete_organization_number'
         }), name = 'organization_number'),
-    path('number-all/<int:org_id>', views.Organization_numberViewSet.as_view({'get':'list_organization_numbers'}), name = 'list_organization_numbers'),
+    path('number-all/<int:org_id>/', views.Organization_numberViewSet.as_view({'get':'list_organization_numbers'}), name = 'list_organization_numbers'),
 
 
     path('link/', views.Organization_linkViewSet.as_view({
@@ -21,7 +21,7 @@ urlpatterns = [
         'patch':'update_organization_link',
         'delete':'delete_organization_link'
         }), name = 'organization_link'),
-    path('link-all/<int:org_id>', views.Organization_numberViewSet.as_view({'get':'list_organization_links'}), name = 'list_organization_links'),
+    path('link-all/<int:org_id>/', views.Organization_numberViewSet.as_view({'get':'list_organization_links'}), name = 'list_organization_links'),
 
 
     path('member/', views.Organization_memberViewSet.as_view({
@@ -30,7 +30,7 @@ urlpatterns = [
         'patch':'update_organization_member',
         'delete':'delete_organization_member'
         }), name = 'organization_member'),
-    path('member-all/<int:org_id>', views.Organization_memberViewSet.as_view({'get':'list_organization_members'}), name = 'list_organization_members'),
+    path('member-all/<int:org_id>/', views.Organization_memberViewSet.as_view({'get':'list_organization_members'}), name = 'list_organization_members'),
 
 
     path('role/', views.RolePermViewSet.as_view({
@@ -38,8 +38,8 @@ urlpatterns = [
         'patch':'update_role',
         'delete':'delete_role'
         }), name = 'organization_role'),
-    path('role-all/<int:org_id>', views.RolePermViewSet.as_view({'get':'list_roles'}), name = 'list_roles'),
-    path('perms-all/<int:org_id>', views.RolePermViewSet.as_view({'get':'list_permissions'}), name = 'list_permissions'),
+    path('role-all/<int:org_id>/', views.RolePermViewSet.as_view({'get':'list_roles'}), name = 'list_roles'),
+    path('perms-all/<int:org_id>/', views.RolePermViewSet.as_view({'get':'list_permissions'}), name = 'list_permissions'),
 
 
     path('service/', views.ServiceViewSet.as_view({
@@ -48,7 +48,7 @@ urlpatterns = [
         'patch':'update_service',
         'delete':'delete_service'
         }), name = 'organization_service'),
-    path('service-all/<int:org_id>', views.ServiceViewSet.as_view({'get':'list_organization_services'}), name = 'list_organization_services'),
+    path('service-all/<int:org_id>/', views.ServiceViewSet.as_view({'get':'list_organization_services'}), name = 'list_organization_services'),
 
 
     path('client/', views.ClientViewSet.as_view({
@@ -56,7 +56,7 @@ urlpatterns = [
         'patch':'update_client',
         'delete':'delete_client'
         }), name = 'organization_client'),
-    path('client-all/<int:org_id>', views.ClientViewSet.as_view({'get':'list_clients'}), name = 'list_clients'),
+    path('client-all/<int:org_id>/', views.ClientViewSet.as_view({'get':'list_clients'}), name = 'list_clients'),
 
 
     path('order/', views.OrderViewSet.as_view({
@@ -64,6 +64,6 @@ urlpatterns = [
         'patch':'update_order',
         'delete':'delete_order'
         }), name = 'organization_order'),
-    path('order-all/<int:org_id>', views.OrderViewSet.as_view({'get':'list_orders'}), name = 'list_orders'),
+    path('order-all/<int:org_id>/', views.OrderViewSet.as_view({'get':'list_orders'}), name = 'list_orders'),
     path('order-block/', views.OrderViewSet.as_view({'post':'block_order'}), name = 'block_order'),
 ]
