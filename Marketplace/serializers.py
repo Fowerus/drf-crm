b12bfaf190bf7d18fe0fbbe17851dd25d987ded1
Fowerus <fowerus@gmail.com>
2021-12-20 23:20:26 +0700

    Add MprodMProduct, MCourier and MOrder views

--- Marketplace/serializers.py
+++ Marketplace/serializers.py
@@ -6,6 +6,52 @@ from restapi.views import get_userData
 
 
 
+class MProductSerializer(serializers.ModelSerializer):
+
+	class MProductCSerializer(serializers.ModelSerializer):
+		organization = serializers.JSONField()
+
+		class Meta:
+			model = MProduct
+			fields = ['name', 'count', 'price', 'price_opt', 'url_product', 'url_photo', 'address', 'provider_site', 'organization']
+
+	class MProductUSerializer(serializers.ModelSerializer):
+		organization = serializers.JSONField(read_only = True)
+
+		class Meta:
+			model = MProduct
+			fields = ['name', 'count', 'price', 'price_opt', 'url_product', 'url_photo', 'address', 'provider_site', 'organization']
+
+	class Meta:
+		model = MProduct
+		fields = ['_id', 'name', 'count', 'price', 'price_opt', 'url_product', 'url_photo', 'address', 'provider_site', 'organization']
+
+
+
+
+class MOrderSerializer(serializers.ModelSerializer):
+
+	class MOrderCSerializer(serializers.ModelSerializer):
+		organization = serializers.JSONField()
+
+		class Meta:
+			model = MOrder
+			fields = ['price', 'address', 'description', 'comment', 'products', 'courier', 'author', 'organization']
+
+	class MOrderUSerializer(serializers.ModelSerializer):
+		organization = serializers.JSONField(write_only = True)
+
+		class Meta:
+			model = MOrder
+			fields = ['price', 'address', 'description', 'comment', 'products', 'courier', 'author', 'organization']
+
+	class Meta:
+		model = MOrder
+		fields = ['_id', 'price', 'address', 'description', 'comment', 'products', 'courier', 'author', 'organization']
+
+
+
+
 class MCourierSerializer(serializers.ModelSerializer):
 
 	class MCourierCSerializer(serializers.ModelSerializer):
--- Marketplace/urls.py
+++ Marketplace/urls.py
@@ -4,6 +4,15 @@ from . import views
 
 
 urlpatterns = [
+	path('morder-c/', views.MOrderCreateAPIView.as_view(), name = 'morder_c'),
+
+
+	path('mproduct-l/<int:organization>/', views.MProductListAPIView.as_view(), name = 'mproduct_l'),
+	path('mproduct-r/<str:_id>/<int:organization>/', views.MProductRetrieveAPIView.as_view(), name = 'mproduct_r'),
+	path('mproduct-c/', views.MProductCreateAPIView.as_view(), name = 'mproduct_c'),
+	path('mproduct-ud/<str:_id>/', views.MProductUpdateDestroyAPIView.as_view(), name = 'mproduct_ud'),
+
+
 	path('mcourier-l/<int:organization>/', views.MCourierListAPIView.as_view(), name = 'mcourier_l'),
 	path('mcourier-c/', views.MCourierCreateAPIView.as_view(), name = 'mcourier_c'),
 	path('mcourier-d/<str:_id>/', views.MCourierDestroyAPIView.as_view(), name = 'mcourier_d'),
